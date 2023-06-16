55bf99a64f0014dfd80e1cc22260547a4d71a2bc
MdAhnafArshad <ahnafarshad.me@gmail.com>
2023-06-16 17:56:33 +0600

    server data from mongo db data set

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const cors = require('cors');
 const dotenv = require('dotenv')
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+const { application } = require('express');
 
 const app = express();
 const port =  process.env.PORT || 5000
@@ -48,12 +49,44 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+
+
+
+     // all operations are bottom side.
+     const database = client.db("carDoctor").collection("services"); 
+
+
+    //  all data find 
+     app.get("/services", async(req, res)=> {
+         const cursor = database.find();
+         const result = await cursor.toArray();
+         console.log(result);
+         res.send(result);
+     })
+
+
+     //specific one data fiend 
+     app.get('/services/:id', async(req, res)=> {
+        const id = req.params.id;
+        const query = {_id : new ObjectId(id)};
+
+        // options 
+        const options = {
+            // Include only the `title` and `imdb` fields in the returned document
+            projection: { title: 1, price: 1, service_id: 1 },
+          };
+
+        const result = await database.findOne(query, options);
+        res.send(result);
+
+     })
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
-    await client.close();
+    // await client.close();
   }
 }
 run().catch(console.dir);
